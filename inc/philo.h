817d51c9b4615c938cb44270512fda0ba1620e1e
m0d1nst4ll3r <raphaelpohlen@gmail.com>
2026-02-15 02:35:14 +0100

    Planning pretty much done, can ask for 2nd opinions but should be good as it is. Header file populated. ETA 1 long day if my planning was good.

--- inc/philo.h
+++ inc/philo.h
@@ -6,59 +6,138 @@
 /*   By: rapohlen <rapohlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/14 14:58:24 by rapohlen          #+#    #+#             */
-/*   Updated: 2026/02/14 16:08:21 by rapohlen         ###   ########.fr       */
+/*   Updated: 2026/02/15 02:26:43 by rapohlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
+# define SUPERVISOR_USLEEP	1000 // Limits how often supervisor checks on philos
+
 # define DEFAULT_ERR	"Unknown error"
 # define ERR_MAL		"Malloc error"
 # define ERR_TIME		"gettimeofday error"
 # define ERR_ARG		"Arguments have to be unsigned integers"
 
-// Libft
-# include "libft.h" // TODO Remove libft eventually and bake functions into program
-// Printf
-# include <stdio.h>
-// Pthread
-# include <pthread.h>
-// Time
-# include <sys/time.h>
-// Bool
-# include <stdbool.h>
-// Errno/strerror
-# include <errno.h>
-# include <string.h>
+// TODO Remove libft eventually and bake functions into program
+# include "libft.h"		// ft_atox, ft_free, ft_time_diff
+# include <stdio.h>		// printf
+# include <pthread.h>	// pthread
+# include <sys/time.h>	// gettimeofday
+# include <stdbool.h>	// bool
+# include <errno.h>		// errno
+# include <string.h>	// strerror
 
-// Time values expressed in ms
-typedef struct s_time
+//	Philosopher struct
+// Exists in an array. Each element is passed to its corresponding philo thread.
+// This is done so threads data is compartmentalized as per subject guidelines.
+// Supervisor has access to everything but only reads values,
+//	except for is_end_of_sim.
+//
+//	id				id used for printing and strategy
+// mutex:
+//	lfork			left fork
+//	rfork			right fork
+//	print			for printf
+//	end				for is_end_of_sim
+//	meals			for meals_eaten
+//	death_time		for death time
+// time:
+//	to_die			value for calculating death time after starting eating
+//	to_eat			value for usleep() after starting eating
+//	to_sleep		value for usleep() after finished eating
+//	meals_eaten		how many meals eaten
+//	current			current time, compared with last_action
+//	last_action		when last action started, used for printing
+//					also used for keeping to sim schedule (if possible)
+//	death			updates on eating, checked by supervisor to determine death
+//	start			time when philosophers all started together (for printing)
+//	is_end_of_sim	set by supervisor when it's time to quit
+//
+// Any address means the value is shared and not specific to this philosopher.
+// Values shared between philos are only mutexes.
+// Values tracked by supervisor are meals_eaten and death time.
+// Value set by supervisor is is_end_of_sim.
+// Start time is an address because it has to be updated after threads launch,
+//	by the main program thread preparing the sim. It is shared with other
+//	philos, but only read, thus no need for a mutex.
+typedef struct s_philo_mutex
 {
-	unsigned int	die;
-	unsigned int	eat;
-	unsigned int	sleep;
-	unsigned int	eat_limit;
-	bool			is_eat_limit_set;
+	pthread_mutex_t	*lfork;
+	pthread_mutex_t	*rfork;
+	pthread_mutex_t	*print;
+	pthread_mutex_t	*end;
+	pthread_mutex_t	meals;
+	pthread_mutex_t	death_time;
+}	t_philo_mutex;
+
+typedef struct s_philo_time
+{
+	unsigned int	to_die;
+	unsigned int	to_eat;
+	unsigned int	to_sleep;
+	unsigned int	meals_eaten;
 	struct timeval	current;
-}	t_time;
+	struct timeval	last_action;
+	struct timeval	death;
+	struct timeval	*start;
+	bool			*is_end_of_sim;
+}	t_philo_time;
 
 typedef struct s_philo
 {
-	int		id;
-	char	fork;
+	int				id;
+	t_philo_mutex	mutex;
+	t_philo_time	time;
 }	t_philo;
 
-//	God struct
-// num_philo		number of philosophers read from args
-// time				time-related values including current time
-// philosophers		array of philosophers (and their forks),
-//					malloc'd in parse_args
+//	Program struct
+//
+//	num_philos		number of philosophers from params
+//	threads			philo threads
+// philos:
+//	Philosophers array containing their data (see philosopher struct)
+// time:
+//	to_die			if a philo has not started eating for this long, it dies
+//	to_eat			how long a philo holds forks after starting eating
+//	to_sleep		how long a philo sleeps after finishing eating
+//	meals_to_end	how many meals each philosopher has to eat to end the sim
+//	start			when the simulation started
+//	current			current time set and used by the supervisor
+//	is_end_of_sim	set to true when philos have eaten enough or one dies
+// mutex:
+//	forks			forks distributed to philos (their lfork and rfork)
+//	print			used for printf calls, philos and supervisor both use this
+//	end				used for is_end_of_sim, supervisor writes, philos read
+//
+// Philo-specific mutexes for values tracked by the supervisor
+//	(death time, meals_eaten) are found in the philos array.
+typedef struct s_time
+{
+	unsigned int	to_die;
+	unsigned int	to_eat;
+	unsigned int	to_sleep;
+	unsigned int	meals_to_end;
+	struct timeval	start;
+	struct timeval	current;
+	bool			is_end_of_sim;
+}	t_time;
+
+typedef struct s_mutex
+{
+	pthread_mutex_t	*forks;
+	pthread_mutex_t	print;
+	pthread_mutex_t	end;
+}	t_mutex;
+
 typedef struct s_prog
 {
-	unsigned int	num_philo;
+	unsigned int	num_philos;
+	pthread_t		*threads;
+	t_philo			*philos;
 	t_time			time;
-	t_philo			*philo;
+	t_mutex			mutex;
 }	t_prog;
 
 // init_exit.c
@@ -69,5 +148,8 @@ void	error_out(t_prog *d, char *err_str);
 
 // args.c
 void	parse_args(t_prog *d, char **av);
+
+// prepare_sim.c
+void	prepare_sim(t_prog *d);
 
 #endif
--- src/args.c
+++ src/args.c
@@ -6,7 +6,7 @@
 /*   By: rapohlen <rapohlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/14 15:23:51 by rapohlen          #+#    #+#             */
-/*   Updated: 2026/02/14 19:53:53 by rapohlen         ###   ########.fr       */
+/*   Updated: 2026/02/14 23:32:06 by rapohlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,15 @@
 
 void	parse_args(t_prog *d, char **av)
 {
-	d->time.is_eat_limit_set = false;
-	if (av[5])
-		d->time.is_eat_limit_set = true;
-	if (ft_atox(av[1], 0, &d->num_philo, sizeof(d->num_philo) | ATOX_U) < 0
-		|| ft_atox(av[2], 0, &d->time.die, sizeof(d->time.die) | ATOX_U) < 0
-		|| ft_atox(av[3], 0, &d->time.eat, sizeof(d->time.eat) | ATOX_U) < 0
-		|| ft_atox(av[4], 0, &d->time.sleep, sizeof(d->time.sleep) | ATOX_U) < 0
-		|| (av[5] && ft_atox(av[5], 0, &d->time.eat_limit,
-				sizeof(d->time.eat_limit) | ATOX_U) < 0))
+	d->time.meal_limit = 0;
+	if (ft_atox(av[1], 0, &d->num_philos, sizeof(d->num_philos) | ATOX_U) < 0
+		|| ft_atox(av[2], 0, &d->time.to_die,
+			sizeof(d->time.to_die) | ATOX_U) < 0
+		|| ft_atox(av[3], 0, &d->time.to_eat,
+			sizeof(d->time.to_eat) | ATOX_U) < 0
+		|| ft_atox(av[4], 0, &d->time.to_sleep,
+			sizeof(d->time.to_sleep) | ATOX_U) < 0
+		|| (av[5] && ft_atox(av[5], 0, &d->time.meals_to_end,
+				sizeof(d->time.meal_to_end) | ATOX_U) < 0))
 		error_out(d, ERR_ARG);
 }
