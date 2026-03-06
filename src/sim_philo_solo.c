eec002e8b309f3d4d412c5fde6aeaf2cd9a6a7db
Raphael Pohlen <rapohlen@f6r9s4.clusters.42paris.fr>
2026-03-06 15:29:04 +0100

    Also added thinking print to stay consistent

--- src/sim_philo_solo.c
+++ src/sim_philo_solo.c
@@ -6,7 +6,7 @@
 /*   By: rapohlen <rapohlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 15:23:20 by rapohlen          #+#    #+#             */
-/*   Updated: 2026/03/06 15:24:45 by rapohlen         ###   ########.fr       */
+/*   Updated: 2026/03/06 15:28:28 by rapohlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	solo_philo(t_prog *d)
 	if (!d->stop)
 	{
 		gettimeofday(&d->time.current, NULL);
+		printf("%d %d is thinking\n", ft_time_sub(d->time.current,
+				d->time.start) / 1000, d->philo_id + 1);
 		printf("%d %d has taken a fork\n", ft_time_sub(d->time.current,
 				d->time.start) / 1000, d->philo_id + 1);
 	}
